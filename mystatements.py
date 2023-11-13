c445eca99914f20a553b53d4461eac5d5fcd1604
Chamberlain <chamberlain.mbah.f@gmail.com>
2023-11-13 09:04:35 +0000

    adding files to repo

---
+++ mystatements.py
@@ -0,0 +1,10 @@
+my_list = ['Apple','cherry','lemon']
+my_dict = {'drinks':'coke','food':'pizza','fruit':'apple'}
+
+for item in my_list:
+    print(f'My favorite fruit to eat is {item}')
+    
+for key,value in my_dict.items():
+    print(f'My favorite {key} is {value}')
+    
+
