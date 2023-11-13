7a6bce4df25962a44a4ad945078415f54437e9c3
Chamberlain <chamberlain.mbah.f@gmail.com>
2023-11-13 12:46:36 +0000

    small changes

--- test_bot.py
+++ test_bot.py
@@ -1,5 +1,8 @@
 import wikipedia
 from mylib.bot import scrape
- 
+
+
 def test_scrape():
-    assert scrape(name='Microsoft',length=1) == wikipedia.summary('Microsoft', sentences=1)
+    assert scrape(name="Microsoft", length=1) == wikipedia.summary(
+        "Microsoft", sentences=1
+    )
